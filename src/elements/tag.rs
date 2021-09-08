9b9e688b65948ec3dbebd009996700dfce3dfc87
Caden Henrich <cadenhenrich@gmail.com>
2021-09-07 21:11:52 -0400

    Built out basic tile class

--- src/elements/tag.rs
+++ src/elements/tag.rs
@@ -2,32 +2,29 @@ use tui::style::Color;
 
 // Tag used to represent a category of tiles
 pub struct Tag {
-    name: String, // The name of the tag
-    color: Color, // The color of the tag
+    name: String,
+    color: Color,
 }
 
 impl Tag {
-    // Creates a new tag with the given name and color.
+    // Constructor
     fn new(name: String, color: Color) -> Tag {
         Tag {name, color}
     }
 
-    // Sets the name of the tag
+    // Getters and setters
     fn set_name(&mut self, name: String) {
         self.name = name;
     }
 
-    // Sets the color of the tag
     fn set_color(&mut self, color: Color) {
         self.color = color;
     }
 
-    // Returns the tame of the tag
     fn get_name(&self) -> &String {
         &self.name
     }
 
-    // Returns the color of the tag
     fn get_color(&self) -> Color {
         self.color
     }
--- src/elements/tile.rs
+++ src/elements/tile.rs
@@ -8,7 +8,38 @@ pub struct Tile {
 }
 
 impl Tile {
+    // Constructor
     fn new(title: String, desc: String, tag: Option<Tag>) -> Tile {
         Tile {title, desc, tag}
+    }
+
+    // Getters and setters
+    fn get_title(&self) -> &String {
+        &self.title
+    }
+
+    fn get_desc(&self) -> &String {
+        &self.desc
+    }
+
+    fn get_tag(&self) -> &Option<Tag> {
+        &self.tag
+    }
+
+    fn set_title(&mut self, title: String) {
+        self.title = title;
+    }
+
+    fn set_desc(&mut self, desc: String) {
+        self.desc = desc;
+    }
+
+    fn set_tag(&mut self, tag: Tag) {
+        self.tag = Some(tag);
+    }
+
+    // Differentiate between some and none
+    fn remove_tag(&mut self) {
+        self.tag = None;
     }
 }
